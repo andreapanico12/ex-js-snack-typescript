d9bf745fabd599aa93029bc92014a60479360ca7
andreapanico12 <andreapanico12@gmail.com>
2025-05-22 09:45:59 +0200

    initialize project with TypeScript and Vite setup, add basic structure and configuration files

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+/* ## Snack 1
+Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
+
+Se è una stringa: stampala in maiuscolo
+
+Se è un numero: moltiplicalo per due e stampalo
+
+Se è un booleano: stampa “Sì” o “No” in base al suo valore
+
+In tutti gli altri casi: stampa “Tipo non supportato”
+
+### BONUS
+Se è null: stampa “Il dato è vuoto”
+Se è un array: stampa la sua lunghezza
+Se è una Promise: attendi che si risolva e stampa il valore del resolve. */
+
+let datoSconosciuto: unknown = await fetchDaApi();
+
+switch (typeof datoSconosciuto) {
+  case "string":
+    console.log(datoSconosciuto.toUpperCase());
+    break;
+
+  case "number":
+    console.log(datoSconosciuto * 2);
+    break;
+
+  case "boolean":
+    console.log(datoSconosciuto ? "Sì" : "No");
+    break;
+
+  default:
+    console.log("Tipo non supportato");
+    break;
+}
